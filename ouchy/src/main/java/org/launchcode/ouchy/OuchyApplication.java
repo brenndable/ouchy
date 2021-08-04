9fa50c94e9a63314c5d0277d1e6563baef736a81
rlp390 <rlp390@hotmail.com>
2021-08-04 15:06:39 -0500

    Initial commit

---
+++ ouchy/src/main/java/org/launchcode/ouchy/OuchyApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.ouchy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OuchyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OuchyApplication.class, args);
+	}
+
+}
